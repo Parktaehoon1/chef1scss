0b8254e7b4dcba8a85a302472635fc70763141ca
Parktaehoon1 <92parkth@gmail.com>
2022-08-19 11:21:44 +0900

    scss 로 진행중

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    let header = $('.header')
+    $(window).scroll(function () {
+        let temp = $(window).scrollTop();
+        if (temp > 100) {
+            header.addClass('header-close');
+        } else {
+            header.removeClass('header-close');
+        }
+    })
+
+
+    let headerOpen = $('.header-btn-open');
+    let headerClose = $('.header-btn-close');
+    let siteMap = $('.sitemap');
+    let siteMapList = $('.sitemap-depth1');
+    let mainHeader = $('.main-header');
+
+    headerOpen.click(function () {
+        headerOpen.addClass('hd-btn-open');
+        headerClose.addClass('hd-btn-close');
+        mainHeader.addClass('main-header-open');
+    })
+    headerClose.click(function () {
+        headerOpen.removeClass('hd-btn-open');
+        headerClose.removeClass('hd-btn-close');
+        mainHeader.removeClass('main-header-open');
+    })
+
+    siteMap.click(function (event) {
+        event.preventDefault();
+        siteMapList.stop().slideToggle(200);
+    })
+
+
+})
+
+window.onload = function () {
+    AOS.init();
+
+
+    //     $("#wrapper").dotdotdot({
+    //         wrapper  : 'div',  /*  콘텐트를 감쌀 요소. */
+    //         ellipsis: '... ',  /*  말줄임표를 뭘로 할지 */
+    //         wrap  : 'word',    /*  자를 단위. 다음 옵션 중 하나 선택: 'word'/'letter'/'children' */
+    //         after  : null,     /*  자르고 나서도 유지시킬 요소를 jQuery 선택자로 적는다. */
+    //         watch  : false,    /*  윈도우가 리사이즈될 때 업데이트할 건지: true/'window' */
+    //         height  : null,     /*  선택. max-height를 지정한다. 만약 null이면 알아서 잰다. */
+    //         tolerance: 0       /*  글이 넘치면 이만큼쯤 height를 늘린다 */
+    //       });
+}
