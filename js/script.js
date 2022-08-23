0455e1b9bd9e3962463e17fdf5b50c1e786536ae
Parktaehoon1 <92parkth@gmail.com>
2022-08-23 17:27:39 +0900

    scrollMagic 적용

--- js/script.js
+++ js/script.js
@@ -2,35 +2,59 @@ $(document).ready(function () {
     let header = $('.header')
     $(window).scroll(function () {
         let temp = $(window).scrollTop();
-        if (temp > 100) {
-            header.addClass('header-close');
-        } else {
-            header.removeClass('header-close');
+        if (temp < 350) {
+            header.removeClass('header-close')
         }
-    })
+    });
 
+    $(window).on('mousewheel', function (e) {
+        let wheel = e.originalEvent.wheelDelta;
+
+        //스크롤값을 가져온다.
+        if (wheel > 0) {
+            //스크롤 올릴때
+            header.removeClass('header-close');
+            // console.log("올라간다")
+        } else if (wheel < 350) {
+            //스크롤 내릴때
+            header.addClass('header-close');
+            // console.log("내려간다")
+        }
+    });
 
     let headerOpen = $('.header-btn-open');
     let headerClose = $('.header-btn-close');
     let siteMap = $('.sitemap');
     let siteMapList = $('.sitemap-depth1');
     let mainHeader = $('.main-header');
+    let mainHeaderOpen = $('main-header-open');
 
     headerOpen.click(function () {
         headerOpen.addClass('hd-btn-open');
         headerClose.addClass('hd-btn-close');
         mainHeader.addClass('main-header-open');
+        // $('body').css('overflow-y', 'hidden');
     })
     headerClose.click(function () {
         headerOpen.removeClass('hd-btn-open');
         headerClose.removeClass('hd-btn-close');
         mainHeader.removeClass('main-header-open');
+        // $('body').css('overflow-y', 'auto');
     })
+
+    // 이 구문이 왜 안되는지?
+    // if(mainHeader.hasClass('main-header-open')){
+    //     $('body').attr('overflow-y', 'hidden');
+    // } else{
+    //     $('body').attr('overflow-y', 'auto');
+    // }
+
 
     siteMap.click(function (event) {
         event.preventDefault();
         siteMapList.stop().slideToggle(200);
     })
+
 
 
     new Swiper('.sw-service', {
@@ -50,6 +74,29 @@ $(document).ready(function () {
 
     swPrev.click(function () {})
     swNext.click(function () {})
+
+
+    (function () {
+        var controller = new ScrollMagic.Controller({
+            globalSceneOptions: {
+                triggerHook: ".productbanner"
+            }
+        });
+
+        new ScrollMagic
+            .Scene({
+                triggerElement: ".productimg",
+                duration: "100%"
+            })
+            .setTween(".productimg", {
+                y: "30%",
+                ease: Linear.easeNone
+            })
+            // .addIndicators()  이거하면 옆에 트리거나옴
+            .addTo(controller);
+    }())
+
+
 })
 
 window.onload = function () {
@@ -66,9 +113,29 @@ window.onload = function () {
     //         tolerance: 0       /*  글이 넘치면 이만큼쯤 height를 늘린다 */
     //       });
 
+    // let mainHeader = document.querySelector('.main-header');
+    // console.log("mainheader",mainHeader)
+    // let body = document.querySelector('body');
+    // console.log("body",body)
+    // let HeaderBtn = document.querySelector('.header-btn-open')
+    // console.log("headerbtn",HeaderBtn)
+    // let HeaderCloseBtn = document.querySelector('.header-btn-close')
+    // console.log("headerClosebtn",HeaderCloseBtn)
 
+    // HeaderBtn.addEventListener('click', function(){
+    //     let temp = mainHeader.hasAttribute('main-header-open');
+    //     console.log('이게뭘까',temp)
+    //     if(mainHeader.hasAttribute('main-header-open')){
+    //         body.style.overflowY = "hidden"
+    //     } else {
+    //         body.style.overflowY = "auto"
+    //     }
+    // });
 
-
-
+    // HeaderCloseBtn.addEventListener('click', function(){
+    //     if(mainHeader.hasAttribute('main-header-open')){
+    //         body.style.overflowY = "auto"
+    //     }
+    // });
 
 }
